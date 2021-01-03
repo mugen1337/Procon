542c29911eefb5007d1459b05526f80b5d2e5c39
mugen1337 <mugen1337@outlook.jp>
2021-01-03 22:49:16 +0900

    test test

---
+++ test/yuki1601.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://yukicoder.me/problems/1601"
+
+#include "../template.cpp"
+
+#include "../DataStructure/RangeSet.cpp"
+
+signed main(){
+    ll d,q;cin>>d>>q;
+    RangeSet<ll> st;
+    ll res=0;
+    while(q--){
+        ll l,r;cin>>l>>r;
+        st.insert(l,r);
+        auto p=st.covered_by(l);
+        chmax(res,p.second-p.first+1);
+        cout<<res<<"\n";
+    }
+    return 0;
+}
