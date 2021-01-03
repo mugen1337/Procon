512387aadc0d30e4951df0c8f0d44c47cda8c010
mugen1337 <mugen1337@outlook.jp>
2021-01-03 23:14:21 +0900

    fix iroiro

---
+++ test/yosupo_Point-Add-Range-Sum.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include "../template.cpp"
+
+#include "../SegmentTree/SegmentTree.cpp"
+
+signed main(){
+    int n,q;cin>>n>>q;
+
+    auto segfunc=[](ll a,ll b){
+        return a+b;
+    };
+    
+    SegmentTree<ll> seg(n,0,segfunc);
+    rep(i,n){
+        ll a;cin>>a;
+        seg.set(i,a);
+    }
+    seg.build();
+
+    for(;q--;){
+        int type;cin>>type;
+        if(type==0){
+            int p;ll x;cin>>p>>x;
+            seg.update(p,seg.query(p,p+1)+x);
+        }
+        else{
+            int l,r;cin>>l>>r;
+            cout<<seg.query(l,r)<<endl;
+        }
+    }
+    return 0;
+}
