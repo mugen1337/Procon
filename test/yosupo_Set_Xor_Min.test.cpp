881f2b16557ae1f81ee356d22b65ef0d85f7369a
mugen1337 <mugen1337@outlook.jp>
2021-01-16 01:11:47 +0900

    add BinaryTrie test

---
+++ test/yosupo_Set_Xor_Min.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
+
+#include "../template.cpp"
+
+#include "../DataStructure/BinaryTrie.cpp"
+
+signed main(){
+    BinaryTrie<int,31> trie;
+    int q;cin>>q;
+    while(q--){
+        int t,x;cin>>t>>x;
+        if(t==0){
+            if(trie.count(x)==0) trie.add(x);
+        }else if(t==1){
+            if(trie.count(x)) trie.erase(x,1);
+        }else{
+            cout<<trie.min_element(x)<<"\n";
+        }
+    }
+    return 0;
+}
