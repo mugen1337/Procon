33560771cd2efbc0f9225f2e4fcbb8fc7ea68a9d
mugen1337 <mugen1337@outlook.jp>
2021-01-18 16:35:57 +0900

    add Graph2/CycleDetec

---
+++ Graph2/CycleDetection.cpp
@@ -0,0 +1,37 @@
+#include "./GraphTemplate.cpp"
+
+template<typename T>
+vector<int> CycleDetection(Graph<T> &g){
+    int n=(int)g.size();
+    vector<int> check(n,0),cyc,pre(n,-1);
+
+    function<bool(int)> dfs=[&](int cur){
+        check[cur]=1;
+        for(auto &to:g[cur]){
+            if(check[to]==0){
+                pre[to]=cur;
+                if(dfs(to)) return true;
+            }else if(check[to]==1){// detect
+                int v=cur;
+                while(v!=to){
+                    cyc.push_back(v);
+                    v=pre[v];
+                }
+                cyc.push_back(v);
+                return true;
+            }
+        }
+        check[cur]=2;
+        return false;
+    };
+
+    for(int i=0;i<n;i++){
+        if(check[i]==0){
+            if(dfs(i)){
+                reverse(begin(cyc),end(cyc));
+                return cyc;
+            }
+        }
+    }
+    return {};
+}
---
+++ test/yosupo_CycleDetection2.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cycle_detection"
+
+#include "../template.cpp"
+
+#include "../Graph2/CycleDetection.cpp"
+
+signed main(){
+    int n,m;cin>>n>>m;
+    Graph g(n);
+    map<pair<int,int>,int> id;
+    rep(i,m){
+        int u,v;cin>>u>>v;
+        id[{u,v}]=i;
+        g.add_directed_edge(u,v);
+    }
+    auto res=CycleDetection(g);
+    if(res.empty()){
+        cout<<-1<<endl;
+        return 0;
+    }
+    int p=res[0];
+    vector<int> ans;
+    for(int i=1;i<(int)res.size();i++){
+        ans.push_back(id[{p,res[i]}]);
+        p=res[i];
+    }
+    ans.push_back(id[{p,res[0]}]);
+    cout<<ans.size()<<"\n";
+    for(auto e:ans) cout<<e<<"\n";
+    return 0;
+}
