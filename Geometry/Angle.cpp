5ea949747226f17a7f69b0fef8c6509137cba233
mugen1337 <53204695+mugen1337@users.noreply.github.com>
2021-07-12 17:10:40 +0900

    Update Angle.cpp

--- Geometry/Angle.cpp
+++ Geometry/Angle.cpp
@@ -1,6 +1,6 @@
 // angle of a-b-c
 Real get_smaller_angle(Point a,Point b,Point c){
-    Point v=b-a,w=c-b;
+    Point v=a-b,w=c-b;
     auto A=atan2(imag(v),real(v));
     auto B=atan2(imag(w),real(w));
     if(A>B) swap(A,B);
