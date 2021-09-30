73b4e0e6b6049f472daac755d5dcb5c0452ea98b
mugen1337 <mugen1337@outlook.jp>
2021-09-30 23:03:26 +0900

    dij

--- Graph2/Dijkstra.cpp
+++ Graph2/Dijkstra.cpp
@@ -5,13 +5,14 @@ struct Dijkstra{
     const T inf;
     Graph<T> g;
     vector<T> d;
-    vector<int> prev;
+    vector<int> prev,eid;
     
-    Dijkstra(Graph<T> g):inf(numeric_limits<T>::max()/2),g(g){}
+    Dijkstra(Graph<T> g):inf(numeric_limits<T>::max()/4),g(g){}
 
     vector<T> build(int st){
         d.assign(g.V,inf);
         prev.assign(g.V,-1);
+        eid.assign(g.V,-1);
         d[st]=0;
         priority_queue<pair<T,int>,vector<pair<T,int>>,greater<pair<T,int>>> que;
         que.emplace(d[st],st);
@@ -29,11 +30,14 @@ struct Dijkstra{
         }
         return d;
     }
-    
-    vector<int> get_path(int gl){
+
+    // vertex = false :-> edge idx
+    vector<int> get_path(int gl,bool vertex=true){
         vector<int> ret;
-        if(d[g]==inf) return ret;
-        for(;gl!=-1;gl=prev[gl]) ret.push_back(gl);
+        if(d[gl]==inf) return ret;
+        for(;gl!=-1;gl=prev[gl]){
+            ret.push_back(vertex?gl:eid[gl]);
+        }
         reverse(ret.begin(),ret.end());
         return ret;
     }
