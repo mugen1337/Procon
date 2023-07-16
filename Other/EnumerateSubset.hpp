92870c19e1d25476303796171986a948e8e644b1
mugen1337 <mugen1337@outlook.jp>
2023-07-16 20:47:40 +0900

    fix subset

--- Other/EnumerateSubset.hpp
+++ Other/EnumerateSubset.hpp
@@ -1,13 +1,10 @@
-vector<int> enumerate_subset(int bit,bool include_bit_empty=false){
-    vector<int> ret;
-    int subset=(bit-1)&bit;
-    do{
-        ret.push_back(subset);
-        subset=(subset-1)&bit;
-    }while(subset!=0);
-    if(include_bit_empty){
-        ret.push_back(0);
-        if(0!=bit) ret.push_back(bit);
+vector< int > enumerate_subset(int S){
+    vector< int > ret;
+    int sub = S;
+    while (sub > 0)
+    {
+        ret.push_back(sub);
+        sub = (sub - 1) & S;
     }
     return ret;
 }
